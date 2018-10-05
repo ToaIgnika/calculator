95353d7dd740386654fa26858cc82d9bcf89a566
toaignika <toaignika@node-1w7jr9qiiblfh10y2x4njioau.ipv6.telus.net>
2018-10-04 23:03:06 -0700

    Initial Commit

---
+++ calculator 2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculator 2.0
+//
+//  Created by toaignika on 2018-10-04.
+//  Copyright © 2018 toaignika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
